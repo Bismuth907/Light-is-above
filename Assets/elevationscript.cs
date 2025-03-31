3d556a4c0b281beb63ed0c5f752c3f2628f1a377
ray liam <rayliammeriadoc@gmail.com>
2025-03-31 17:04:32 +0200

    oui

---
+++ Assets/elevationscript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
+public class elevationscript : MonoBehaviour
+{
+    Vector2 movement = Vector2.zero;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+    }
+    void OnCollisionStay2D(Collision2D collision) 
+    {
+    }
+
+}
