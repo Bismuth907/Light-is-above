83777e755d4e41a3e4873e5da3c1bf7a4309b2e2
ray liam <rayliammeriadoc@gmail.com>
2025-05-02 11:55:11 +0200

    les plateform qui bougent fonctionne presque

--- Assets/plateformscript.cs
+++ Assets/plateformscript.cs
@@ -8,16 +8,25 @@ using UnityEngine.UI;
 public class plateformscript : MonoBehaviour
 {
     public Rigidbody2D rigidbody2D;
+    public float relPos;
     void Start ()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        relPos = transform.position.x - collision.gameObject.transform.position.x;
     }
     void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("plateform"))
+        if (collision.gameObject.CompareTag("plateform") && rigidbody2D.linearVelocity.x == 0f)
         { 
-         rigidbody2D.velocity = collision.gameObject.velocity;
+         transform.position = new Vector2 (collision.transform.position.x + relPos, transform.position.y);
+        }
+        if (collision.gameObject.CompareTag("plateform") && rigidbody2D.linearVelocity.x != 0f)
+        {
+            relPos = transform.position.x - collision.gameObject.transform.position.x;
         }
     }
-
 }
